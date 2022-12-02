c993c35446ff23afc660d199dbf6664796e365b0
kdoni <chrisvdoni@gmail.com>
2022-12-02 15:44:08 +0200

    It could have been done in under 3 lines...

--- fibs_recursion.rb
+++ fibs_recursion.rb
@@ -1,11 +1,18 @@
-def fibs_rec(number, result = [])
-  result.push(number)
+def fibs_rec_tsk(number)
   if number < 2
     number
   else
-    fibs_rec(number - 1) + fibs_rec(number - 2)
+    fibs_rec_tsk(number - 1) + fibs_rec_tsk(number - 2)
   end
-  result
 end
 
+def fibs_rec(number)
+  result = []
+  while number >= 0
+    result.push(fibs_rec_tsk(number))
+    number -= 1
+  end
+  result.shift
+  result.reverse
+end
 p fibs_rec(8)
