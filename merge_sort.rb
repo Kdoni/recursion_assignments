6094b872c22d6c70021d2c96dec96ae0d27b31cd
kdoni <chrisvdoni@gmail.com>
2022-12-03 13:19:58 +0200

    I still do not get it...

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,24 +2,25 @@ def merge_sort(array)
   if array.length < 2
     array
   else
-    first_half = array.slice(0, array.length / 2)
-    second_half = array.slice(array.index(first_half.last), array.index(array.last))
-    merge(merge_sort(first_half), merge_sort(second_half))
+    half = (array.length / 2).floor
+    first_half = merge_sort(array[0..half - 1])
+    second_half = merge_sort(array[half..array.length])
+    merge(first_half, second_half)
   end
 end
 
-def merge(left, right)
-  if left.empty?
-    right
-  elsif right.empty?
-    left
-  elsif left.first < right.first
-    [left.first] + merge(left[1..left.length], right)
+def merge(first, second)
+  if first.empty?
+    second
+  elsif second.empty?
+    first
+  elsif first.first < second.first
+    [first.first] + merge(first[1..first.length], second)
   else
-    [right.first] + merge(left, right[1..right.length])
+    [second.first] + merge(first, second[1..second.length])
   end
 end
 
-test = [5, 3, 6, 3, 2, 1, 7]
+test = [5, 3, 4]
 
 p merge_sort(test)
